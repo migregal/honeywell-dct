aaa5a8f0e637fcca2a310bd854007035028b901a
migregal <devmigregal@gmail.com>
2021-07-03 16:11:05 +0300

    WebChromeClient simplified

--- app/src/main/java/com/finnflare/dct/honeywell/WebChromeClient.kt
+++ app/src/main/java/com/finnflare/dct/honeywell/WebChromeClient.kt
@@ -1,30 +1,7 @@
 package com.finnflare.dct.honeywell
 
-import android.app.AlertDialog
 import android.content.Context
-import android.webkit.JsResult
 import android.webkit.WebChromeClient
-import android.webkit.WebView
 
 
-class BaseWebChromeClient(private val context: Context): WebChromeClient() {
-    override fun onJsConfirm(
-        view: WebView?,
-        url: String?,
-        message: String?,
-        result: JsResult
-    ): Boolean {
-        AlertDialog.Builder(context)
-            .setTitle("App Titler")
-            .setMessage(message)
-            .setPositiveButton(
-                R.string.ok,
-                { dialog, which -> result.confirm() })
-            .setNegativeButton(
-                R.string.cancel,
-                { dialog, which -> result.cancel() })
-            .create()
-            .show()
-        return true
-    }
-}
+class BaseWebChromeClient(private val context: Context) : WebChromeClient() {}
